1ace173e1565e80ee3f74f6d67930ad02e342d5b
Jerome <j.patrick.wiz@gmail.com>
2016-07-06 04:44:14 +0800

    Added more tests to LeaveCreditsTest

--- src/main/java/com/orangeandbronze/leaveapp/domain/LeaveCredits.java
+++ src/main/java/com/orangeandbronze/leaveapp/domain/LeaveCredits.java
@@ -1,33 +1,29 @@
 package com.orangeandbronze.leaveapp.domain;
 
 public class LeaveCredits {
-	public static final LeaveCredits ZERO = new LeaveCredits.Builder().build();
-	
-	private final float vacationLeaveCreditsIncreaseRate = (float) 1.25;
-	private final float sickLeaveCreditsIncreaseRate = (float) 1.25;
-	
 	private float vacationLeaveCredits;
 	private float sickLeaveCredits;
 	private float emergencyLeaveCredits;
-	private float offsetLeaveCredits;
 	private float soloParentLeaveCredits;
+	private float offsetLeaveCredits;
+	
+	private float lwopCount;
 	
 	public static class Builder{
 		private float vacationLeaveCredits = 0;
 		private float sickLeaveCredits = 0;
 		private float emergencyLeaveCredits = 0;
-		private float offsetLeaveCredits = 0;
 		private float soloParentLeaveCredits = 0;
+		private float offsetLeaveCredits = 0;
+		private float lwopCount = 0;
 		
-		public Builder(){
-			
-		}
+		public Builder() { }
 		
 		public Builder vacationLeaveCredits(float value){
 			vacationLeaveCredits = value;
 			return this;
 		}
-
+		
 		public Builder sickLeaveCredits(float value){
 			sickLeaveCredits = value;
 			return this;
@@ -38,99 +34,131 @@ public class LeaveCredits {
 			return this;
 		}
 		
-		public Builder offsetLeaveCredits(float value){
-			offsetLeaveCredits = value;
-			return this;
-		}
-		
 		public Builder soloParentLeaveCredits(float value){
 			soloParentLeaveCredits = value;
 			return this;
 		}
 		
-		public LeaveCredits build(){
+		public Builder offsetLeaveCredits(float value){
+			offsetLeaveCredits = value;
+			return this;
+		}
+		
+		public Builder lwopCount(float value) {
+			lwopCount = value;
+			return this;
+		}
+
+		public LeaveCredits build() {
 			return new LeaveCredits(this);
 		}
 	}
-	
+
 	private LeaveCredits(Builder builder) {
 		this.vacationLeaveCredits = builder.vacationLeaveCredits;
 		this.sickLeaveCredits = builder.sickLeaveCredits;
 		this.emergencyLeaveCredits = builder.emergencyLeaveCredits;
-		this.offsetLeaveCredits = builder.offsetLeaveCredits;
 		this.soloParentLeaveCredits = builder.soloParentLeaveCredits;
-	}
-	
-	public float getLeaveCreditsOfType(LeaveType type){
-		switch (type) {
-		case VACATION_LEAVE:
-			return vacationLeaveCredits;
-		case SICK_LEAVE:
-			return sickLeaveCredits;
-		case EMERGENCY_LEAVE:
-			return emergencyLeaveCredits;
-		case SOLO_PARENT_LEAVE:
-			return soloParentLeaveCredits;
-		case OFFSET_LEAVE:
-			return offsetLeaveCredits;
-		default:
-			return 0;
-		}
+		this.offsetLeaveCredits = builder.offsetLeaveCredits;
+		this.lwopCount = builder.lwopCount;
 	}
 
-	public float getOffsetLeaveCredits() {
-		return offsetLeaveCredits;
+	public float getVacationLeaveCredits() {
+		return vacationLeaveCredits;
+	}
+
+	public float getSickLeaveCredits() {
+		return sickLeaveCredits;
+	}
+
+	public float getEmergencyLeaveCredits() {
+		return emergencyLeaveCredits;
 	}
 
 	public float getSoloParentLeaveCredits() {
 		return soloParentLeaveCredits;
 	}
 
+	public float getOffsetLeaveCredits() {
+		return offsetLeaveCredits;
+	}
+
+	public float getLwopCount() {
+		return lwopCount;
+	}
+
 	public void increaseVacationLeaveCredits() {
-		vacationLeaveCredits += vacationLeaveCreditsIncreaseRate;
+		vacationLeaveCredits += 1.25;
+	}
+
+	public float getLeaveCreditsOfType(LeaveType type) {
+		switch (type) {
+		case VACATION_LEAVE: return getVacationLeaveCredits();
+		case SICK_LEAVE: return getSickLeaveCredits();
+		case EMERGENCY_LEAVE: return getEmergencyLeaveCredits();
+		case SOLO_PARENT_LEAVE: return getSoloParentLeaveCredits();
+		case OFFSET_LEAVE: return getOffsetLeaveCredits();
+		default:
+			return 0;
+		}
 	}
 
 	public void increaseSickLeaveCredits() {
-		sickLeaveCredits += sickLeaveCreditsIncreaseRate;
+		sickLeaveCredits += 1.25;
 	}
 
 	public void deductLeaveCreditsOfType(LeaveType type, float creditsToDeduct) {
 		switch (type) {
-		case SICK_LEAVE:
-			deductSickLeaveCreditsBy(creditsToDeduct);
-			break;
-		case VACATION_LEAVE:
-			deductVacationLeaveCreditsBy(creditsToDeduct);
-			break;
-		case EMERGENCY_LEAVE:
-			deductEmergencyLeaveCreditsBy(creditsToDeduct);
-			break;
-		case SOLO_PARENT_LEAVE:
-			deductSoloParentLeaveCreditsBy(creditsToDeduct);
-			break;
-		case OFFSET_LEAVE:
-			deductOffsetLeaveCreditsBy(creditsToDeduct);
+		case VACATION_LEAVE: deductVacationLeaveCredits(creditsToDeduct); break;
+		case SICK_LEAVE: deductSickLeaveCredits(creditsToDeduct); break;
+		case EMERGENCY_LEAVE: deductEmergencyLeaveCredits(creditsToDeduct); break;
+		case SOLO_PARENT_LEAVE: deductSoloParentLeaveCredits(creditsToDeduct); break;
+		case OFFSET_LEAVE: deductOffsetLeaveCredits(creditsToDeduct); break;
 		}
 	}
 
-	private void deductOffsetLeaveCreditsBy(float creditsToDeduct) {
+	private void deductOffsetLeaveCredits(float creditsToDeduct) {
 		offsetLeaveCredits -= creditsToDeduct;
+		if(isNegative(offsetLeaveCredits)){
+			lwopCount += creditsToDeduct;
+			offsetLeaveCredits = 0;
+		}
 	}
 
-	private void deductSoloParentLeaveCreditsBy(float creditsToDeduct) {
+	private void deductSoloParentLeaveCredits(float creditsToDeduct) {
 		soloParentLeaveCredits -= creditsToDeduct;
+		if(isNegative(soloParentLeaveCredits)){
+			deductVacationLeaveCredits(creditsToDeduct);
+			soloParentLeaveCredits = 0;
+		}
 	}
 
-	private void deductEmergencyLeaveCreditsBy(float creditsToDeduct) {
+	private void deductEmergencyLeaveCredits(float creditsToDeduct) {
 		emergencyLeaveCredits -= creditsToDeduct;
+		if(isNegative(emergencyLeaveCredits)){
+			deductVacationLeaveCredits(creditsToDeduct);
+			emergencyLeaveCredits = 0;
+		}
 	}
 
-	private void deductVacationLeaveCreditsBy(float creditsToDeduct) {
+	private void deductVacationLeaveCredits(float creditsToDeduct) {
 		vacationLeaveCredits -= creditsToDeduct;
+		if(isNegative(vacationLeaveCredits)){
+			lwopCount += creditsToDeduct;
+			vacationLeaveCredits = 0;
+		}
 	}
 
-	private void deductSickLeaveCreditsBy(float creditsToDeduct) {
+	private boolean isNegative(float leaveCredits) {
+		return leaveCredits < 0;
+	}
+
+	private void deductSickLeaveCredits(float creditsToDeduct) {
 		sickLeaveCredits -= creditsToDeduct;
+		if(isNegative(sickLeaveCredits)){
+			lwopCount += creditsToDeduct;
+			sickLeaveCredits = 0;
+		}
 	}
 	
 }
--- src/test/java/com/orangeandbronze/leaveapp/domain/LeaveCreditsTest.java
+++ src/test/java/com/orangeandbronze/leaveapp/domain/LeaveCreditsTest.java
@@ -1,6 +1,6 @@
-package com.orangeandbronze.leaveapp.model;
+package com.orangeandbronze.leaveapp.domain;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -69,6 +69,57 @@ public class LeaveCreditsTest {
 		credits = builder.emergencyLeaveCredits(3).build();
 		credits.deductLeaveCreditsOfType(LeaveType.EMERGENCY_LEAVE, 1);
 		assertLeaveCreditsOfALeaveTypeEvaluatesTo(LeaveType.EMERGENCY_LEAVE, 2);
+	}
+	
+	@Test
+	public void deductingOffsetLeaveCredits() throws Exception {
+		credits = builder.offsetLeaveCredits(5).build();
+		credits.deductLeaveCreditsOfType(LeaveType.OFFSET_LEAVE, 1);
+		assertLeaveCreditsOfALeaveTypeEvaluatesTo(LeaveType.OFFSET_LEAVE, 4);
+	}
+	
+	@Test
+	public void exceedingVacationLeaveDaysShouldIncreasLWOPCount() throws Exception {
+		credits = builder.build();
+		credits.deductLeaveCreditsOfType(LeaveType.VACATION_LEAVE, 3);
+		assertLeaveCreditsOfALeaveTypeEvaluatesTo(LeaveType.VACATION_LEAVE, 0);
+		assertLWOPCountEvaluatesTo(3);
+	}
+	
+	@Test
+	public void exceedingEmergencyLeaveDaysShouldBeDeductedFromVacationLeaveCredits() throws Exception {
+		credits = builder.vacationLeaveCredits(9).build();
+		credits.deductLeaveCreditsOfType(LeaveType.EMERGENCY_LEAVE, 3);
+		assertLeaveCreditsOfALeaveTypeEvaluatesTo(LeaveType.EMERGENCY_LEAVE, 0);
+		assertLeaveCreditsOfALeaveTypeEvaluatesTo(LeaveType.VACATION_LEAVE, 6);
+	}
+	
+	@Test
+	public void exceedingSoloParentLeaveDaysShouldBeDeductedFromVacationLeaveCredits() throws Exception {
+		credits = builder.vacationLeaveCredits(9).build();
+		credits.deductLeaveCreditsOfType(LeaveType.SOLO_PARENT_LEAVE, 3);
+		assertLeaveCreditsOfALeaveTypeEvaluatesTo(LeaveType.SOLO_PARENT_LEAVE, 0);
+		assertLeaveCreditsOfALeaveTypeEvaluatesTo(LeaveType.VACATION_LEAVE, 6);
+	}
+	
+	@Test
+	public void exceedingSickLeaveDaysShouldIncreaseLWOPCount() throws Exception {
+		credits = builder.build();
+		credits.deductLeaveCreditsOfType(LeaveType.SICK_LEAVE, 3);
+		assertLeaveCreditsOfALeaveTypeEvaluatesTo(LeaveType.SICK_LEAVE, 0);
+		assertLWOPCountEvaluatesTo(3);
+	}
+	
+	@Test
+	public void exceedingOffsetLeaveDaysShouldIncreaseLWOPCount() throws Exception {
+		credits = builder.build();
+		credits.deductLeaveCreditsOfType(LeaveType.OFFSET_LEAVE, 3);
+		assertLeaveCreditsOfALeaveTypeEvaluatesTo(LeaveType.OFFSET_LEAVE, 0);
+		assertLWOPCountEvaluatesTo(3);
+	}
+
+	private void assertLWOPCountEvaluatesTo(float lwopCount) {
+		assertTrue(3 == credits.getLwopCount());
 	}
 
 }
